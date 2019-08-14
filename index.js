a1e5e518357bb10b5f7553f56457dd52c8f9a885
Andrea Mandini <a.mandini@mauti.it>
2019-08-14 17:12:23 +0200

    feat(wololo): add random wololo message in page title

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 (function () {
+
+    // USING MERSENNETWISTER TO GENERATE A CONSISTENT RANDOM REAL
+    let MersenneTwister = require('mersenne-twister'),
+    generator = new MersenneTwister();
+
     Object.defineProperty(window, 'new_title', {
         set(val) {
-            document.title = val;
-            value = val;
+
+            generator.random() > 0.8 
+            ? (value = val)
+            : (value = "WOLOLO")
+
+            document.title = value            
         }
     })
 })()
